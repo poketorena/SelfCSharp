3fb1be8a406fab3065d45ba707801c10e9b7e36b
poketorena <poketorena@users.noreply.github.com>
2018-02-01 12:54:48 +0900

    コマンドライン引数とforeach

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -11,22 +11,9 @@ namespace SelfCSharp
     {
         static void Main(string[] args)
         {
-            int score = 75;
-            if (score >= 90)
+            foreach (var item in args)
             {
-                Console.WriteLine("優");
-            }
-            else if (score >= 70)
-            {
-                Console.WriteLine("良");
-            }
-            else if (score >= 50)
-            {
-                Console.WriteLine("可");
-            }
-            else
-            {
-                Console.WriteLine("不可");
+                Console.WriteLine($"こんにちは{item}さん！");
             }
         }
     }
