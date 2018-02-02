83c909eb6028259b5678b100603520cc2d83a7f9
poketorena <poketorena@users.noreply.github.com>
2018-02-02 18:08:29 +0900

    正規表現を使って郵便番号を取り出すプログラム

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SelfCSharp
@@ -10,12 +11,14 @@ namespace SelfCSharp
     {
         static void Main(string[] args)
         {
-            var str = "鈴木\t太郎\t男\t50歳\t広島県";
-            var result = str.Split('\t');
+            var str = "住所は〒184-0000 鎌ヶ谷市梶野町0-0-0です。\nあなたの住所は〒273-0000 懦恋市大野町0-9-9ですね";
+            var rgx = new Regex(@"\d{3}-\d{4}");
+            var result = rgx.Matches(str);
             foreach (var item in result)
             {
                 Console.WriteLine(item);
             }
+
         }
     }
 }
