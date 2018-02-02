05f81f217c3a382e1e3ba73a981fad7c9f5b2491
poketorena <poketorena@users.noreply.github.com>
2018-02-02 18:29:35 +0900

    正規表現でメールアドレスを置換する

--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -11,14 +11,9 @@ namespace SelfCSharp
     {
         static void Main(string[] args)
         {
-            var str = "住所は〒184-0000 鎌ヶ谷市梶野町0-0-0です。\nあなたの住所は〒273-0000 懦恋市大野町0-9-9ですね";
-            var rgx = new Regex(@"\d{3}-\d{4}");
-            var result = rgx.Matches(str);
-            foreach (var item in result)
-            {
-                Console.WriteLine(item);
-            }
-
+            var str = "お問い合わせはhoge@example.comまで";
+            var rgx = new Regex(@"[a-z_0-9]+([-+.][a-z_0-9]+)*@[a-z_0-9]+([-.][a-z_0-9]+)*\.[a-z_0-9]+([-.][a-z_0-9]+)*", RegexOptions.IgnoreCase);
+            Console.WriteLine(rgx.Replace(str, "<a href=\"mailto:$0\">$0</a>"));
         }
     }
 }
