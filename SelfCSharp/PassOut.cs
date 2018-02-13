1a2e6566c828c44359fa943d56940ff8a8cb897d
poketorena <poketorena@users.noreply.github.com>
2018-02-13 20:14:59 +0900

    メソッドから値を返す時に使うoutキーワード
    refキーワードと同様に参照渡しされる

---
+++ SelfCSharp/PassOut.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using SelfCSharp;
+
+namespace SelfCSharp
+{
+    class PassOut
+    {
+        public void GetMaxMin(int x, int y, out int max, out int min)
+        {
+            if (x >= y)
+            {
+                max = x;
+                min = y;
+            }
+            else
+            {
+                max = y;
+                min = x;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var a = new PassOut();
+            int resultMax, resultMin;
+            a.GetMaxMin(5, 3, out resultMax, out resultMin);
+            Console.WriteLine(resultMax);
+            Console.WriteLine(resultMin);
+        }
+    }
+}
