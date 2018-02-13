dc282d5637ab12944150140fe7723fa4c70f3ca4
poketorena <poketorena@users.noreply.github.com>
2018-02-13 18:06:51 +0900

    既定値（デフォルト値）の練習

---
+++ SelfCSharp/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCSharp
+{
+    class Circle
+    {
+        double radius;
+
+        public Circle(double radius = 1)
+        {
+            this.radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.Pow(radius, 2) * Math.PI;
+        }
+    }
+}
--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -13,7 +13,11 @@ namespace SelfCSharp
     {
         static void Main(string[] args)
         {
-            var s = SelfCSharp.MySingleton.Instance;
+            var radius = 3.0;
+            var c1 = new Circle();
+            var c2 = new Circle(radius);
+            Console.WriteLine(c1.GetArea());
+            Console.WriteLine(c2.GetArea());
         }
     }
 }
