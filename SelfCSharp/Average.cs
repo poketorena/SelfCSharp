a42415616aad2d1819c964ca6b450c646db6013e
poketorena <poketorena@users.noreply.github.com>
2018-02-13 18:14:46 +0900

    可変長引数の練習
    クラスメソッドもあるよ

---
+++ SelfCSharp/Average.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCSharp
+{
+    static class Average
+    {
+        public static double GetAverage(params double[] values)
+        {
+            double sum = 0;
+            foreach (var item in values)
+            {
+                sum += item;
+            }
+            return sum / values.Length;
+        }
+    }
+}
--- SelfCSharp/Program.cs
+++ SelfCSharp/Program.cs
@@ -13,11 +13,8 @@ namespace SelfCSharp
     {
         static void Main(string[] args)
         {
-            var radius = 3.0;
-            var c1 = new Circle();
-            var c2 = new Circle(radius);
-            Console.WriteLine(c1.GetArea());
-            Console.WriteLine(c2.GetArea());
+            var avg = Average.GetAverage(1, 2, 3, 4, 5);
+            Console.WriteLine(avg);
         }
     }
 }
