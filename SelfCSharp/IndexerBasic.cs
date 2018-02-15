21ca2f476cfd7f73eb876fe01bd3e89e48509678
poketorena <poketorena@users.noreply.github.com>
2018-02-15 10:18:10 +0900

    インデクサーの練習

--- SelfCSharp/IndexerBasic.cs
+++ SelfCSharp/IndexerBasic.cs
@@ -9,14 +9,21 @@ using SelfCSharp;
 
 namespace SelfCSharp
 {
-    class Keisan
+    class IndexerBasic
     {
         public static (double addition, double subtract) AddSubtract(double x, double y) => (x + y, x - y);
         static void Main(string[] args)
         {
-            var tuple = Keisan.AddSubtract(5, 3);
-            Console.WriteLine(tuple.addition);
-            Console.WriteLine(tuple.subtract);
+            var array = new FreeArray(5);
+            array[0] = 1;
+            array[1] = 10;
+            array[2] = 15;
+            array[3] = 30;
+            array[4] = 60;
+
+            Console.WriteLine(array[2]);
+            Console.WriteLine(array[-10]);
+            Console.WriteLine(array[6]);
         }
     }
 }
