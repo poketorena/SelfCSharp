4bc1ab2471d2a6d2c83c9eb91c3b9307b3773e10
poketorena <poketorena@users.noreply.github.com>
2018-03-18 11:43:11 +0900

    練習問題9.2

--- SelfCSharp/Main.cs
+++ SelfCSharp/Main.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using SelfCSharp.Chap08.ExtensionBasic;
 using SelfCSharp;
 
 namespace SelfCSharp
@@ -14,8 +13,6 @@ namespace SelfCSharp
     {
         static void Main(string[] args)
         {
-            var c = new Coordinates(35.681167, 35.681167);
-            Console.WriteLine(c);
         }
     }
 }
