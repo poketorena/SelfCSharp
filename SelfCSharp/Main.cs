04be8a2cbc2370498c1e032852e04940b8e74241
poketorena <poketorena@users.noreply.github.com>
2018-03-19 17:39:48 +0900

    第10章の理解度チェック2-4（クエリ構文）

--- SelfCSharp/Main.cs
+++ SelfCSharp/Main.cs
@@ -16,10 +16,10 @@ namespace SelfCSharp
         delegate TResult Foo<in T, out TResult>(T v1, T v2);
         static void Main(string[] args)
         {
-            var data = AppTables.Books
-                .Where(x => x.Title.EndsWith("入門"))
-                .OrderByDescending(x => x.Price)
-                .Select(x => new { Title = x.Title, Price = x.Price * 1.08 });
+            var data = from b in AppTables.Books
+                       where b.Title.EndsWith("入門")
+                       orderby b.Price descending
+                       select new { Title = b.Title, Price = b.Price * 1.08 };
             foreach (var item in data)
             {
                 Console.WriteLine("Title:" + item.Title + " Price:" + item.Price);
