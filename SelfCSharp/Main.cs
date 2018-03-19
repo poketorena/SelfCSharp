e49299f4143dc2766b165f57c71bb74e09bb6054
poketorena <poketorena@users.noreply.github.com>
2018-03-19 16:58:31 +0900

    それぞれの要素から先頭3文字だけを切り出したリストを作る

--- SelfCSharp/Main.cs
+++ SelfCSharp/Main.cs
@@ -11,10 +11,17 @@ namespace SelfCSharp
 {
     class DelegateAnonymous
     {
+        delegate bool Hoge(string str);
+
+        delegate TResult Foo<in T, out TResult>(T v1, T v2);
         static void Main(string[] args)
         {
-            var data = new List<string> { "あかまきがみ", "あおまきがみ", "きまきがみ" };
-            Console.WriteLine(data.TrueForAll(x => x.Length <= 5));
+            var data = new List<string> { "ABCDE", "OPQR", "WXYZ", "HIJKL" };
+            var list = data.Select(x => x.Substring(0, 3));
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
