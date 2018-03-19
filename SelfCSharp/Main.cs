c37b3e8971dbe2f48230bf9956fa94b015b82598
poketorena <poketorena@users.noreply.github.com>
2018-03-19 15:42:51 +0900

    List<sting> の内容がすべて5文字以内であるかを調べる

--- SelfCSharp/Main.cs
+++ SelfCSharp/Main.cs
@@ -11,39 +11,10 @@ namespace SelfCSharp
 {
     class DelegateAnonymous
     {
-        // string型の引数を受け取り、戻り値はvoidであるデリゲート
-        delegate string OutputProcess(string str);
-
-        // 配列要素の処理方法をデリゲート経由で受け取れるように
-        void ArrayWalk(string[] data, Func<string,string> output)
-        {
-            foreach (var value in data)
-            {
-                Console.WriteLine(output(value));
-            }
-        }
-
-        // OutputProcess型に対応したメソッド
-        static string AddQuote(string data)
-        {
-            return $"[{data}]";
-        }
-
-        static string Front4(string data)
-        {
-            return data.Substring(0, 4);
-        }
-
         static void Main(string[] args)
         {
-            var data = new[] { "あかまきがみ", "あおまきがみ", "きまきがみ" };
-            var dm = new DelegateAnonymous();
-
-            // デリゲート型の引数に匿名メソッドを渡す
-            dm.ArrayWalk(data, delegate (string d)
-             {
-                 return $"[{d}]";
-             });
+            var data = new List<string> { "あかまきがみ", "あおまきがみ", "きまきがみ" };
+            Console.WriteLine(data.TrueForAll(x => x.Length <= 5));
         }
     }
 }
