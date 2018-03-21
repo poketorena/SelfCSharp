68d06097f5eb01c50c0ed066f38c2926cc2ff073
poketorena <poketorena@users.noreply.github.com>
2018-03-21 12:12:56 +0900

    値を返す非同期メソッド

--- SelfCSharp/Main.cs
+++ SelfCSharp/Main.cs
@@ -7,32 +7,35 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using SelfCSharp;
 using System.Threading;
+using System.Diagnostics;
 
 namespace SelfCSharp
 {
-    class AsyncBasic
+    class AsyncRegturn
     {
         static void Main(string[] args)
         {
-            // 非同期メソッドを呼び出す
-            Task t = RunAsync();
-            Console.WriteLine("...他の処理...");
-            t.Wait();
-        }
-
-        // 非同期メソッドを定義
-        static async Task RunAsync()
-        {
-            await Task.Run(() => Count(1));
-            Console.WriteLine("処理が完了しました。");
-        }
-
-        static void Count(int n)
-        {
-            for (int i = 0; i < 50; i++)
+            // 非同期メソッドの呼び出し
+            Task<TimeSpan> t = RunAsync();
+            // 非同期メソッドの終了待ち
+            while (!t.IsCompleted)
             {
-                Console.WriteLine($"Task{n}:{i}");
+                t.Wait(200);
+                Console.Write(".");
             }
+            Console.WriteLine(t.Result);
+        }
+
+        // 戻り値のある非同期メソッド
+        static async Task<TimeSpan> RunAsync()
+        {
+            var watch = Stopwatch.StartNew();
+            await Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+            });
+            watch.Stop();
+            return watch.Elapsed;
         }
     }
 }
