773aa65268356eae822f1088aef7dd87b5bfb7d9
poketorena <poketorena@users.noreply.github.com>
2018-03-21 12:28:32 +0900

    非同期メソッドの利用例
    ネットワーク上の指定されたコンテンツを取得、コンソールに表示する例（非同期Mainを使用 - C#7.1）

--- SelfCSharp/Main.cs
+++ SelfCSharp/Main.cs
@@ -8,34 +8,17 @@ using System.Threading.Tasks;
 using SelfCSharp;
 using System.Threading;
 using System.Diagnostics;
+using System.Net;
 
 namespace SelfCSharp
 {
-    class AsyncRegturn
+    class AsyncHttp
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            // 非同期メソッドの呼び出し
-            Task<TimeSpan> t = RunAsync();
-            // 非同期メソッドの終了待ち
-            while (!t.IsCompleted)
-            {
-                t.Wait(200);
-                Console.Write(".");
-            }
-            Console.WriteLine(t.Result);
-        }
-
-        // 戻り値のある非同期メソッド
-        static async Task<TimeSpan> RunAsync()
-        {
-            var watch = Stopwatch.StartNew();
-            await Task.Run(() =>
-            {
-                Thread.Sleep(5000);
-            });
-            watch.Stop();
-            return watch.Elapsed;
+            var client = new WebClient();
+            var result = await client.DownloadStringTaskAsync("https://codezine.jp/");
+            Console.WriteLine(result);
         }
     }
 }
